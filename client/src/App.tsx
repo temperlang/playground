2c717e848a54608553103536943df9f99c9ee8c6
Tom <tom@temper.systems>
2025-05-13 15:58:36 -0700

    Add monaco editor
    Signed-off-by: Tom <tom@temper.systems>

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,37 @@
 import type { Component } from "solid-js";
+import { MonacoEditor } from "solid-monaco";
+import type * as monacoEditor from "monaco-editor";
 
 import logo from "./assets/temper-logo-256.png";
 import styles from "./App.module.css";
+import defaultSource from "./assets/default.temper?raw";
+
+const TemperEditor = () => {
+  const handleMount = (
+    monaco: typeof monacoEditor,
+    editor: monacoEditor.editor.IStandaloneCodeEditor,
+  ) => {
+    // Doesn't seem to work as an element attribute, so set here.
+    monaco.editor.defineTheme("temper-dark", {
+      base: "vs-dark",
+      inherit: true,
+      rules: [],
+      colors: {
+        "editor.background": "#0a1c35",
+        "editor.lineHighlightBorder": "#162640",
+      },
+    });
+    monaco.editor.setTheme("temper-dark");
+    editor.updateOptions({ minimap: { enabled: false } });
+  };
+  return (
+    <MonacoEditor
+      onMount={handleMount}
+      language="typescript"
+      value={defaultSource}
+    />
+  );
+};
 
 const App: Component = () => {
   return (
@@ -18,6 +48,17 @@ const App: Component = () => {
           Learn Solid
         </a> */}
       </header>
+      <div class={styles.toolbar}>
+        <div class={styles.devTools}>Build</div>
+        <div class={styles.metaTools}>Share</div>
+      </div>
+      <div class={styles.workArea}>
+        <div class={styles.sourceArea}>
+          <TemperEditor />
+        </div>
+        <div class={styles.resultArea}>Result Area</div>
+      </div>
+      <div class={styles.toolbar}>Legal Stuff</div>
     </div>
   );
 };
