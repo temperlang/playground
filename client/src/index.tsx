a7eb510c9920862b82eb2b1d9be625d2fb37cd3f
Tom <tom@temper.systems>
2025-06-03 16:04:57 -0700

    Add google analytics to prod
    Signed-off-by: Tom <tom@temper.systems>

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -4,6 +4,28 @@ import { render } from "solid-js/web";
 import "./index.css";
 import App from "./App";
 
+declare global {
+  interface Window {
+    dataLayer: any[];
+  }
+}
+
+if (import.meta.env.PROD) {
+  // Analytics support external script.
+  const gtagScript = document.createElement("script");
+  gtagScript.async = true;
+  gtagScript.src = "https://www.googletagmanager.com/gtag/js?id=G-3T0PZK7D3F";
+  document.head.appendChild(gtagScript);
+  // And our own call.
+  window.dataLayer = window.dataLayer || [];
+  function gtag(...args: any[]): void;
+  function gtag() {
+    window.dataLayer.push(arguments);
+  }
+  gtag("js", new Date());
+  gtag("config", "G-3T0PZK7D3F");
+}
+
 const root = document.getElementById("root");
 
 if (import.meta.env.DEV && !(root instanceof HTMLElement)) {
